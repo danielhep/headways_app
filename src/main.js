d67a6fcd61d1f5976103bd8d860c00a34468d2f5
Daniel Heppner <dh@danielheppner.com>
2020-02-25 17:46:17 -0800

    added vcalendar

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 
 import './assets/styles/index.css'
 import 'normalize.css'
+import 'vue-slider-component/theme/default.css'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faMapMarkerAlt, faRoute, faClock, faMapMarker, faMap, faPlane, faTimes, faCaretLeft, faCheckSquare } from '@fortawesome/free-solid-svg-icons'
@@ -12,9 +13,10 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { gqlProvider } from './vue-apollo'
 import VueElementLoading from 'vue-element-loading'
 import VueSlider from 'vue-slider-component'
-import 'vue-slider-component/theme/default.css'
 import VueInputAutowidth from 'vue-input-autowidth'
+import VCalendar from 'v-calendar'
 
+Vue.use(VCalendar)
 Vue.component('VueSlider', VueSlider)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.component('vue-element-loading', VueElementLoading)
