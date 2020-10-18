a28f7ff0d25e3001c548748f20e1a97b0408bcce
Daniel Heppner <daniel.heppner1@gmail.com>
2020-10-18 16:06:14 -0700

    organize dashboard by agency

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -9,16 +9,24 @@
           Routes
         </div>
         <div
-          v-for="route in feed.routes"
-          :key="route._id"
-          class="flex justify-start content-center cursor-pointer hover:text-accent-1 hover:bg-gray-lighter px-2 py-2"
+          v-for="agency in feed.agencies"
+          :key="agency.agency_name"
         >
-          <span class="bg-gray-400 h-2 w-2 m-2 rounded-full" />
-          <div class="flex-grow font-medium px-2 flex items-center">
-            {{ route.route_short_name }}
+          <div class="py-2 pr-4 text-bold text-right bg-gray-lighter border-t border-b border-accent-1 sticky top-0">
+            {{ agency.agency_name }}
           </div>
-          <div class="text-sm font-normal text-gray-300 tracking-wide flex items-center">
-            {{ route.route_long_name ? route.route_long_name : route.route_desc }}
+          <div
+            v-for="route in agency.routes"
+            :key="route._id"
+            class="flex justify-start content-center cursor-pointer hover:text-accent-1 hover:bg-gray-lighter px-2 py-2"
+          >
+            <span class="bg-gray-400 h-2 w-2 m-2 rounded-full" />
+            <div class="flex-grow font-medium px-2 flex items-center">
+              {{ route.route_short_name }}
+            </div>
+            <div class="text-sm font-normal text-gray-300 tracking-wide flex items-center">
+              {{ route.route_long_name ? route.route_long_name : route.route_desc }}
+            </div>
           </div>
         </div>
       </div>
@@ -40,11 +48,14 @@ export default {
       query: gql`
         query($feedIndex: Int!) {
           feed(feed_index: $feedIndex) {
-            routes {
-              route_long_name
-              route_short_name
-              route_desc
-              _id
+            agencies {
+              agency_name
+              routes {
+                route_long_name
+                route_short_name
+                route_desc
+                _id
+              }
             }
           }
         }
